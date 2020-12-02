c52d8f62c527fbb8f710b6f162737b275a7b00f9
György Bence Józsa <jozsagyorgy@edu.bme.hu>
2020-12-02 16:55:19 +0100

    Added API key to constants

--- app/src/main/java/hu/bme/aut/andwallet/AppConstants.kt
+++ app/src/main/java/hu/bme/aut/andwallet/AppConstants.kt
@@ -4,4 +4,5 @@ object AppConstants {
     const val PREFERENCE = "app"
     const val PIN_PREF = "PIN"
     const val FIRST_LAUNCH_PREF = "firstLaunch"
+    const val API_KEY = "800S7H8QCQRK9NJB"
 }
